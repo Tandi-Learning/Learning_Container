a70c4935628936f273cfecf06d2f2183a4f533d7
tandisunarto <tandi.sunarto@hotmail.com>
2018-05-17 17:21:34 -0400

    Added a third app

---
+++ docker-network/sample-mvc/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using sample_mvc.Models;
+
+namespace sample_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.TryAddWithoutValidation("content-type", "application/json");
+            var fruits = await client.GetStringAsync("http://sample-api/api/fruits");
+            ViewData["Message"] = fruits;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page......";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
