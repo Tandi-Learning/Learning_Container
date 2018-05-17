a329f33ff1e506b05f42d9ecfeacd73681374667
tandisunarto <tandi.sunarto@hotmail.com>
2018-05-17 14:51:10 -0400

    Updated docker-compose and sample-app

--- docker-network/sample-app/src/Fruits.js
+++ docker-network/sample-app/src/Fruits.js
@@ -3,51 +3,30 @@ import React, { Component } from "react";
 export default class Fruits extends Component 
 {
    constructor(props) {
-      super(props);      
-      this.getFruits();
+      super(props);
    }
 
    state = {
       fruits: []
    }
 
-   getFruits = () => {
-      fetch('http://sample-api/api/fruits', {
-         method: 'get',
-         headers: {
-            accept: 'application/json',
-         }
-      }).then(response => {
-         console.log("FIRST ATTEMPT WORKED");
-      }).catch(error => {
-         console.log("FIRST ATTEMPT FAILED");
-         console.log(error);
-      })
+   componentDidMount() {
+      this.getFruits();
+   }
 
-      fetch('http://sample-api:9090/api/fruits', {
+   getFruits = () => {      
+      fetch('http://localhost:9090/api/fruits', {
          method: 'get',
          headers: {
             accept: 'application/json',
          }
       }).then(response => {
-         console.log("SECOND ATTEMPT WORKED");
-      }).catch(error => {
-         console.log("SECOND ATTEMPT FAILED");
-         console.log(error);
-      })         
-      // })
-      // fetch('http://localhost:9090/api/fruits', {
-      //    method: 'get',
-      //    headers: {
-      //       accept: 'application/json',
-      //    }
-      // }).then(response => {
-      //    return response.json();
-      // }).then(fruits => {
-      //    this.setState({
-      //       fruits: fruits
-      //    })
-      // });
+         return response.json();
+      }).then(fruits => {
+         this.setState({
+            fruits: fruits
+         })
+      });
    }
 
    render() {
